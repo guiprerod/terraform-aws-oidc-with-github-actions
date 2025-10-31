d1c1a929731b2b16326528647ee3b83bc801cf46
guiprerod <guiprerod@outlook.es>
2025-10-31 09:10:29 +0100

    Agregar archivos de configuración de Terraform para la integración de OIDC con GitHub Actions

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "eu-west-2" #TODO: change to your preferred region
+}
+
+
+
+module "oidc-with-github-actions" {
+  source  = "thetestlabs/oidc-with-github-actions/aws"
+  version = "0.1.4"
+
+  github_org = "thetestlabs"
+  github_repositories = [
+    "tfc-ec2",
+    "tfc-aws-vpc",
+  ] #TODO: change to your preferred repositories
+  iam_role_name        = "Example_OIDC_Role"
+  iam_role_description = "Enable GitHub OIDC access"
+  max_session_duration = 3600
+  iam_role_policy      = "AdministratorAccess"
+  iam_role_path        = "/security/"
+}
